0c5ef9dd4ad14d19c23ab70ca3a828af5e7c88e8
Zuicyy <zuicy2021@gmail.com>
2023-12-11 01:16:37 +0545

    Connected it to database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 
 def index(request):
@@ -15,7 +16,13 @@ def gallery(request):
     return render(request, 'gallery.html')
 
 def contact(request):
-    
+    if request.method == "POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+        contact=Contact(name=name, email=email, message=message)
+        contact.save()
+
     return render(request,'contactus.html')
 
 def login(request):
